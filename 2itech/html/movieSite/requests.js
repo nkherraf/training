664887afb6ade8fc23c44d099c53c9e86b6e92b3
Delizo <lorenzo.de.lillo.13@gmail.com>
2021-10-29 16:13:12 +0200

    Ajout2

--- 2itech/html/movieSite/requests.js
+++ 2itech/html/movieSite/requests.js
@@ -15,7 +15,7 @@ var attributsInfo = ["Director","Actors","Runtime","Plot","Released","Genre","Wr
 for(let u=0;u<ul.length;u++) {
     list.push(ul[u].querySelector('span'));
 }
-
+console.log('modified');
 var inputTitle = document.getElementById('srcInput2');
 
 /*navBtn.addEventListener('click', () => {
